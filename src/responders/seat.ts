f8af3a2ffe6bc51e60b57a7d64901d7ce51eb670
Josiah Jones <veteranmina@users.noreply.github.com>
2022-01-11 15:20:21 -0600

    Add thumbnail

--- src/responders/seat.ts
+++ src/responders/seat.ts
@@ -2,6 +2,8 @@ import { Message, MessageEmbed } from 'discord.js'
 import * as log from '../lib/console'
 
 const seataddr = process.env.APP_URL;
+const eveimage = process.env.EVE_IMG;
+const imagetyp = process.env.EVE_IMG_TYPE;
 
 const CALENDAR = `.calendar`;
 const DOCTRINE = `.doctrine`;
@@ -14,6 +16,19 @@ const doctrine = `${seataddr}` + `/fitting/doctrine`;
 const fitting = `${seataddr}` + `/fitting`;
 const srp = `${seataddr}` + `/srp`
 
+if (eveimage === undefined && imagetyp === undefined) {
+    eveimage === `1000001`;
+    imagetyp === `corporations`;
+}
+if (imagetyp === undefined) {
+    imagetyp === `corporations`;
+}
+if (eveimage === undefined) {
+    eveimage === `1000001`;
+}
+
+const botthumbnail = `https://images.evetech.net/${imagetyp}/${eveimage}/logo?size=128`
+
 export const matcher = `!seat`;
 export const handle = (m: Message): void => {
   log.debug(`message from ${m.author.username}. sending an seat related response`);
@@ -23,10 +38,11 @@ export const handle = (m: Message): void => {
   }
   else {
     const mEmbed = new MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle('SeAT Related Links')
+        .setColor(`#0099ff`)
+        .setTitle(`SeAT Related Links`)
+        .setThumbnail(`${botthumbnail}`)
         .addFields(
-            { name: '\u200B', value: `[SeAT Login](${seataddr})\n[Operations Calendar](${calendar})\n[Fleet Doctrines](${doctrine})\n[Ship Fittings](${fitting})\n[Ship SRP](${srp})`},
+            { name: `\u200B`, value: `[SeAT Login](${seataddr})\n[Operations Calendar](${calendar})\n[Fleet Doctrines](${doctrine})\n[Ship Fittings](${fitting})\n[Ship SRP](${srp})`},
             );
     m.channel.send( { content: `${m.author}`, embeds: [mEmbed] });
   }
