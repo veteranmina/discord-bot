fc6d91802bf9d02ca8aa1d7d8460c90d5eaa16e7
VeritasLuxMea <veteranmina@users.noreply.github.com>
2022-05-09 11:34:20 -0500

    Create PM response

---
+++ src/responders/dm.ts
@@ -0,0 +1,16 @@
+import { Message, MessageEmbed } from 'discord.js'
+import * as log from '../lib/console'
+
+const seataddr = process.env.APP_URL;
+
+export const matcher = `!Notice me`;
+export const handle = (m: Message): void => {
+  log.debug(`message from ${m.author.username}. sending an seat related response`);
+
+  if (seataddr === undefined) {
+    m.channel.send(`${m.author}, The SeAT address is currently not set`);
+  }
+  else {
+    m.author.send('Here is the link to authorize discord roles ${seataddr}')
+  }
+};
