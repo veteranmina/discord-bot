dec02208acd8fffef50972c15bfed4c1ed806919
Josiah Jones <veteranmina@users.noreply.github.com>
2022-05-15 21:25:35 -0500

    bing not bong

--- src/responders/bing.ts
+++ src/responders/bing.ts
@@ -1,7 +1,7 @@
 import { Message } from 'discord.js'
 import * as log from '../lib/console'
 
-export const matcher = `!bong`;
+export const matcher = `!bing`;
 export const handle = (m: Message): void => {
     log.debug(`message from ${m.author.username}. sending a pong`);
     m.channel.send(`Bong!`);
