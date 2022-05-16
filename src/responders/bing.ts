55c8bb55804b7f1dba249fb3d5b9a5789baa1f8b
Josiah Jones <veteranmina@users.noreply.github.com>
2022-05-15 21:21:07 -0500

    Bing...request

---
+++ src/responders/bing.ts
@@ -0,0 +1,8 @@
+import { Message } from 'discord.js'
+import * as log from '../lib/console'
+
+export const matcher = `!bong`;
+export const handle = (m: Message): void => {
+    log.debug(`message from ${m.author.username}. sending a pong`);
+    m.channel.send(`Bong!`);
+};
