47e7302fb2a9d6cc80a8b73c04233870f284a637
Josiah Jones <veteranmina@users.noreply.github.com>
2022-05-15 22:00:42 -0500

    cycle back changes...not cats for you

--- src/say.ts
+++ src/say.ts
@@ -4,7 +4,6 @@ import * as log from './lib/console'
 
 import * as ping from './responders/ping'
 import * as bing from './responders/bing'
-import * as cat from './responders/cat'
 import * as eve from './responders/eve'
 import * as seat from './responders/seat'
 import * as tools from './responders/tools'
@@ -18,7 +17,6 @@ export class Handler {
   constructor() {
     this.handlers.push({matcher: ping.matcher, handle: ping.handle}); // bot ping
     this.handlers.push({matcher: bing.matcher, handle: bing.handle}); // bot bing
-    this.handlers.push({matcher: cat.matcher, handle: cat.handle}); // cats
     this.handlers.push({matcher: eve.matcher, handle: eve.handle}); // eve
     this.handlers.push({matcher: seat.matcher, handle: seat.handle}); // seat
     this.handlers.push({matcher: tools.matcher, handle: tools.handle}); // tools
